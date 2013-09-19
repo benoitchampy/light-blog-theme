5f2f43fea2a439d82dd3273cc7c935b6dd2ad86b
BC <bc@vnbc.fr>
2013-09-19 18:38:25 +0200

    Add js to hide adress bar on mobile

--- footer.php
+++ footer.php
@@ -5,6 +5,15 @@
 	
 	<footer></footer>
 	<?php wp_footer(); ?>
-	
+	<script type="text/javascript">
+		// When ready...
+		window.addEventListener("load",function() {
+			// Set a timeout...
+			setTimeout(function(){
+				// Hide the address bar!
+				window.scrollTo(0, 1);
+			}, 0);
+		});
+	</script>
 </body>
 </html>
